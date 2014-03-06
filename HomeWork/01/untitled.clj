b272e10968695162527fb757af2cc7a8b38e9577
unknown <dpavlov@User5-PC.lirex.com>
2014-03-06 17:35:36 +0200

    add untitled.clj

---
+++ HomeWork/01/untitled.clj
@@ -0,0 +1,21 @@
+(defn form-digits [numbers]
+  (read-string
+   (apply str numbers)))
+
+
+(defn digits [number]
+  seq (str number))
+
+
+(form-digits [1 2 3])
+
+
+(digits)
+
+(defn primefactors
+  ([n]
+    (primefactors n 2 '()))
+  ([n candidate acc]
+    (cond (<= n 1) (vec (reverse acc))
+          (zero? (rem n candidate)) (recur (/ n candidate) candidate (cons candidate acc))
+          :else (recur n (inc candidate) acc))))
